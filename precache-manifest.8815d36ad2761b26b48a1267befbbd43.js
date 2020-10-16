19dbe915870357a4a1757f367d485bd45a195c59
TARISO <toni.ariso@gmail.com>
2020-10-16 02:25:26 +0200

    Updates

---
+++ precache-manifest.8815d36ad2761b26b48a1267befbbd43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401ab11eb039ee05055004f2dcea7ccb",
+    "url": "/React-Gif/index.html"
+  },
+  {
+    "revision": "942a1b61e38e3784c2dd",
+    "url": "/React-Gif/static/css/main.7e81ae07.chunk.css"
+  },
+  {
+    "revision": "b0d48a9b93fa07d7e0d1",
+    "url": "/React-Gif/static/js/2.880b3c89.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Gif/static/js/2.880b3c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942a1b61e38e3784c2dd",
+    "url": "/React-Gif/static/js/main.b2a3e8a7.chunk.js"
+  },
+  {
+    "revision": "58e4b08d588d4a0d25b3",
+    "url": "/React-Gif/static/js/runtime-main.0ba8619b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Gif/precache-manifest.8815d36ad2761b26b48a1267befbbd43.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Gif/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
